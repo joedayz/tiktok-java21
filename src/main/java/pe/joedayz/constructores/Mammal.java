99697e5f516b3d7700ac2a50157852fe83a7adfa
Jose Diaz <jose.diaz@joedayz.pe>
2025-10-28 11:51:22 -0500

    fin de constructors

---
+++ src/main/java/pe/joedayz/constructores/Mammal.java
@@ -0,0 +1,24 @@
+package pe.joedayz.constructores;
+
+public class Mammal {
+    public Mammal(int age) {
+
+    }
+}
+
+
+class Seal extends Mammal {
+    public Seal(){
+            super(6);
+    }
+}
+
+class Elephant extends Mammal {
+    public Elephant() {
+        super(4);
+    }
+}
+
+class AfricanElephant extends Elephant {
+
+}
