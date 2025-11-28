4f7a7bb5ae5c54996306ce2beb06d71bddd98d09
Jose Diaz <jose.diaz@joedayz.pe>
2025-11-28 18:58:53 -0500

    final methods

---
+++ src/main/java/pe/joedayz/overriding/Bird.java
@@ -0,0 +1,21 @@
+package pe.joedayz.overriding;
+
+public class Bird {
+
+    public final boolean hasFeathers(){
+        return true;
+    }
+    public final static void flyAway(){
+
+    }
+}
+
+class Penguin extends Bird{
+
+//    public final boolean hasFeathers(){
+//        return true;
+//    }
+//public final static void flyAway(){
+//
+//}
+}
